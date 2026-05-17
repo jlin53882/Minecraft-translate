87b14c5ae54ecf6c3cc32312b1104bbf1455a3cc
jlin53882 <jlin53882@gmail.com>
2026-05-18 00:44:55 +0800

    chore(extractor): improve output dir helper text format

--- app/views/extractor_view.py
+++ app/views/extractor_view.py
@@ -81,7 +81,7 @@ class ExtractorView(ft.Column):
 
         self.output_dir_textfield = ft.TextField(
             hint_text="（未指定將自動產生）",
-            helper="範例：\n• mods + _提取lang_輸出 = mods_提取lang_輸出\n• mods + _預覽book_輸出 = mods_預覽book_輸出\n• mods + _zh_tw_output = mods_zh_tw_output",
+            helper="未指定時自動產生（路徑 + 設定名稱）：\n路徑：C:\\games\\minecraft\\mods + _提取lang_輸出 = C:\\games\\minecraft\\mods_提取lang_輸出\n路徑：C:\\games\\minecraft\\mods + _預覽book_輸出 = C:\\games\\minecraft\\mods_預覽book_輸出\n路徑：C:\\games\\minecraft\\mods + _zh_tw_output = C:\\games\\minecraft\\mods_zh_tw_output",
             expand=True,
             dense=True,
             border_color=theme.OUTLINE,
